aeafcd4809c6f25c280c119b66ab22545de0f3ba
olafnub <googlhoogle@gmail.com>
2025-10-22 00:17:40 -0500

    init

---
+++ uni-v1/foundry-app/src/Token.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier; MIT
+
+pragma solidity ^0.8.25;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    // 1 million tokens minted
+    constructor() ERC20("Token", "TKN") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+}
